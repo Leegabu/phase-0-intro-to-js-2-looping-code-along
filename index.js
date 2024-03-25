8a85b67e35476d2a60b2d5badd7c096b670fb1f2
Lee <leeirungu11313@gmail.com>
2024-03-25 11:15:36 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names =["Guadalupe","Ollie","Aki"]
+function writeCards(names) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      let name = names[i];
+      let message = `Thank you, ${name}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+
+}
+function countDown(){
+    let i = 0;
+
+
+while(i <= 10) {
+    console.log(i)
+    i++
+}
+}
